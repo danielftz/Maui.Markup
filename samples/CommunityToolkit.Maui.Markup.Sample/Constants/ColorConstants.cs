97d9d9cc9dc6f59bd6af7abc1a36c310ff21e2a1
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-12-05 14:19:56 -0800

    `dotnet format`

--- samples/CommunityToolkit.Maui.Markup.Sample/Constants/ColorConstants.cs
+++ samples/CommunityToolkit.Maui.Markup.Sample/Constants/ColorConstants.cs
@@ -4,12 +4,16 @@ namespace CommunityToolkit.Maui.Markup.Sample.Constants;
 
 static class ColorConstants
 {
-    public static Color NavigationBarBackgroundColor { get; } = Color.FromHex("FF6601");
-    public static Color NavigationBarTextColor { get; } = Colors.Black;
+	[System.Obsolete]
+	public static Color NavigationBarBackgroundColor { get; } = Color.FromHex("FF6601");
+	public static Color NavigationBarTextColor { get; } = Colors.Black;
 
-    public static Color TextCellDetailColor { get; } = Color.FromHex("828282");
-    public static Color TextCellTextColor { get; } = Colors.Black;
+	[System.Obsolete]
+	public static Color TextCellDetailColor { get; } = Color.FromHex("828282");
+	public static Color TextCellTextColor { get; } = Colors.Black;
 
-    public static Color BrowserNavigationBarBackgroundColor { get; } = Color.FromHex("FFE6D5");
-    public static Color BrowserNavigationBarTextColor { get; } = Color.FromHex("3F3F3F");
+	[System.Obsolete]
+	public static Color BrowserNavigationBarBackgroundColor { get; } = Color.FromHex("FFE6D5");
+	[System.Obsolete]
+	public static Color BrowserNavigationBarTextColor { get; } = Color.FromHex("3F3F3F");
 }
