c8e5fac45d4f02458580bd3465747ad38173b205
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-12-05 14:51:53 -0800

    Update ColorConstants.cs

--- samples/CommunityToolkit.Maui.Markup.Sample/Constants/ColorConstants.cs
+++ samples/CommunityToolkit.Maui.Markup.Sample/Constants/ColorConstants.cs
@@ -4,16 +4,13 @@ namespace CommunityToolkit.Maui.Markup.Sample.Constants;
 
 static class ColorConstants
 {
-	[System.Obsolete]
-	public static Color NavigationBarBackgroundColor { get; } = Color.FromHex("FF6601");
+	public static Color NavigationBarBackgroundColor { get; } = Color.FromArgb("FF6601");
 	public static Color NavigationBarTextColor { get; } = Colors.Black;
 
-	[System.Obsolete]
-	public static Color TextCellDetailColor { get; } = Color.FromHex("828282");
+	public static Color TextCellDetailColor { get; } = Color.FromArgb("828282");
 	public static Color TextCellTextColor { get; } = Colors.Black;
 
-	[System.Obsolete]
-	public static Color BrowserNavigationBarBackgroundColor { get; } = Color.FromHex("FFE6D5");
-	[System.Obsolete]
-	public static Color BrowserNavigationBarTextColor { get; } = Color.FromHex("3F3F3F");
+	public static Color BrowserNavigationBarBackgroundColor { get; } = Color.FromArgb("FFE6D5");
+
+	public static Color BrowserNavigationBarTextColor { get; } = Color.FromArgb("3F3F3F");
 }
